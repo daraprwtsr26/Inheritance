713c74036b8aedd794ca506ded0780dbf70d0a3a
Daraprwtsr <Daraprwtsr@192.168.56.104>
2017-09-08 11:08:42 +0700

    ModulInheritance

---
+++ src/Latihan1/Class3.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan1;
+
+/**
+ *
+ * @author Daraprwtsr
+ */
+public class Class3 {
+    public static void main (String [] args)
+    {
+        Class2 D2 = new Class2 ();
+        D2.BacaSuper ();
+        D2.info();
+        
+        Class1 S1 = new Class1 ();
+        S1.terprotek();
+        S1.info();
+    }
+}
